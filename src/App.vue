0518cb2ada7185ed10f0e3175a65fb36f5e36ae5
theskyworld <2456450687@qq.com>
2023-12-05 22:25:21 +0800

    feat(VideoShowCard.vue): add VideoShowCard

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import VideoShowCard from "./components/base/videoShowCard/VideoShowCard.vue";
 import HomePage from "./pages/HomePage.vue";
 </script>
 
 <template>
-  <HomePage />
+  <!-- <HomePage /> -->
+  <VideoShowCard />
 </template>
 
 <style scoped></style>
