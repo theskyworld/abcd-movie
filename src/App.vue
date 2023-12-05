e29e4c22718beaff08e31a648aea057ce6f26a06
theskyworld <2456450687@qq.com>
2023-12-05 15:57:44 +0800

    feat(SideMenus.vue): add SideMenus

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import NavBar from "./components/navBar/NavBar.vue";
+import SideMenus from "./components/sideMenus/SideMenus.vue";
 </script>
 
 <template>
   <NavBar />
+  <SideMenus />
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/styles/index.scss";
-import "//at.alicdn.com/t/c/font_4358565_5fzk2s1ht32.js";
+import "//at.alicdn.com/t/c/font_4106328_jc75e3ptrt.js";
 
 createApp(App).mount("#app");
