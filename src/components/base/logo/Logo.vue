1b83e460cea04b493c097b7dbb81c046a853bdb0
theskyworld <2456450687@qq.com>
2023-12-07 03:51:40 +0800

    refactor(Logo.vue): move style to .scss file

--- src/components/base/logo/Logo.vue
+++ src/components/base/logo/Logo.vue
@@ -8,15 +8,5 @@
   </div>
 </template>
 <style scoped lang="scss">
-.container {
-  height: 30px;
-  width: 200px;
-}
-h1 {
-  margin: 0;
-  display: flex;
-  align-items: center;
-  font-size: 1.5em;
-  font-weight: 700;
-}
+@use "./logo.scss";
 </style>
