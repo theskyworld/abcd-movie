a93ca3f830b989db26d1fb76647ff851f77add3b
theskyworld <2456450687@qq.com>
2023-12-07 02:53:34 +0800

    feat(HomePage.vue): toggle navbar style

--- src/pages/homePage/HomePage.vue
+++ src/pages/homePage/HomePage.vue
@@ -6,11 +6,42 @@ import BestMoviesInWeek from "@/components/bestMoviesInWeek/BestMoviesInWeek.vue
 import BestMoviesInMonth from "@/components/bestMoviesInMonth/BestMoviesInMonth.vue";
 import NetflixHot from "@/components/netflixHot/NetflixHot.vue";
 import HomePageSwiper from "../../components/base/homePageSwiper/HomePageSwiper.vue";
+
+import { ref, onMounted } from "vue";
+
+const homeePageSwiperWrapperRef = ref();
+const homePageContainerRef = ref();
+const navBarWrapperRef = ref();
+
+function intersectionObserverSwiper() {
+  const intersectionObserver = new IntersectionObserver((entries) => {
+    homePageContainerRef.value.onscroll = () => {
+      // 如果swiper元素被滑动到了视口之外
+      if (entries[0].intersectionRatio === 0) {
+        // 将navbar的背景颜色进行添加，将其定为改为固定定位
+        navBarWrapperRef.value.style.backgroundColor = "#25252b";
+        navBarWrapperRef.value.style.position = "fixed";
+      }
+      // 如果swiper元素被滑动重新进入了视口
+      if (entries[0].intersectionRatio > 0) {
+        // 将navbar的样式改回
+        navBarWrapperRef.value.style.backgroundColor = "";
+        navBarWrapperRef.value.style.position = "";
+      }
+    };
+  });
+
+  intersectionObserver.observe(homeePageSwiperWrapperRef.value);
+}
+
+onMounted(() => {
+  intersectionObserverSwiper();
+});
 </script>
 
 <template>
-  <div class="home-page-container">
-    <div class="nav-bar-wrapper">
+  <div class="home-page-container" ref="homePageContainerRef">
+    <div class="nav-bar-wrapper" ref="navBarWrapperRef">
       <NavBar />
     </div>
     <Teleport to="body">
@@ -18,7 +49,7 @@ import HomePageSwiper from "../../components/base/homePageSwiper/HomePageSwiper.
         <SideMenus />
       </div>
     </Teleport>
-    <div class="home-page-swiper-wrapper">
+    <div class="home-page-swiper-wrapper" ref="homeePageSwiperWrapperRef">
       <HomePageSwiper />
     </div>
     <div class="video-shows-wrapper">
