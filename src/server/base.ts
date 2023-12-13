f5f0e5bb697e59e740853ef9db3de7198b99622d
theskyworld <2456450687@qq.com>
2023-12-13 12:08:49 +0800

    feat(vite.config.ts): config CORS

--- src/server/base.ts
+++ src/server/base.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL应当为"/api",对应于跨域请求中的配置
+  baseURL: "/api",
 });
 
 export function get(url: string) {
