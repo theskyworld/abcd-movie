e81784eb677ff2541cf4938ef48aee14e733eab0
theskyworld <2456450687@qq.com>
2023-12-15 01:27:09 +0800

    feat: add MovieLibraries

---
+++ src/components/movieLibraries/MovieLibraries.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MovieSelectors from "@/components/base/movieSelectors/MovieSelectors.vue";
+import VideoShowCard from "@/components/base/videoShowCard/VideoShowCard.vue";
+</script>
+<template>
+  <div class="movie-libraries-container">
+    <div class="movie-selectors-wrapper">
+      <MovieSelectors />
+    </div>
+    <div class="video-show-card-wrapper">
+      <VideoShowCard />
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+@use "./movieLibraries.scss";
+</style>
