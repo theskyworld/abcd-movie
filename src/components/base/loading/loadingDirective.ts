889a92f87b2e037753f1955b92eeeb035a994519
theskyworld <2456450687@qq.com>
2023-12-17 23:00:44 +0800

    refactor: change Loading

--- src/components/base/loading/loadingDirective.ts
+++ src/components/base/loading/loadingDirective.ts
@@ -1,6 +1,4 @@
 // loading自定义指令
 import createDirective from "@/assets/ts/createDirective";
-// import Loading from "./Loading.vue";
-import NetflixPageSkeleton from "@/pages/netflixPage/NetflixPageSkeleton.vue";
-
-export default createDirective(NetflixPageSkeleton);
+import Loading from "./Loading.vue";
+export default createDirective(Loading);
