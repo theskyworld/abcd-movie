a279f0a3a888f874bbebb453d3a692d1f0841169
theskyworld <2456450687@qq.com>
2024-01-08 23:29:48 +0800

    feat: add infinity loading datas for NetfixPage

--- src/pages/netflixPage/NetflixPage.vue
+++ src/pages/netflixPage/NetflixPage.vue
@@ -2,13 +2,87 @@
 import VideoShowCard from "@/components/base/videoShowCard/VideoShowCard.vue";
 import Pagination from "@/components/base/pagination/Pagination.vue";
 import getNetflixPageData from "@/server/getNetflixPageData.ts";
-import { ref, onBeforeMount, Ref } from "vue";
+import { ref, onBeforeMount, Ref, onMounted, onUpdated } from "vue";
+import useStorage from "@/utils/useStorage";
+import Loading from "@/components/base/loading/Loading.vue";
+import { createApp } from "vue";
 
-const datas = ref([]);
+const { setStorage, getStorage } = useStorage();
+const curPage = ref(1);
+
+const videoShowCardElemsRef = ref();
+const contentWrapperElemRef = ref();
+const datas = ref<Data | {}>({});
 const isLoading = ref(true);
+
 onBeforeMount(async () => {
-  datas.value = await getNetflixPageData(1);
+  datas.value = await getNetflixPageData(curPage.value);
+  setStorage("netflixPageData", JSON.stringify(datas.value));
   isLoading.value = false;
+});
+
+interface Data {
+  imgURLs: string[];
+  videoTitles: string[];
+  videoScores: string[];
+  videoEpisodes: string[];
+}
+
+onUpdated(() => {
+  // 最后获取30页的数据
+  if (curPage.value < 30) {
+    contentWrapperElemRef.value.onscroll = fn;
+  }
+  function fn() {
+    const observer = new IntersectionObserver(async (entries, observer) => {
+      if (entries[0].intersectionRatio > 0.8) {
+        // 当前侦听的元素（当前所有渲染的卡片中的最后一个）
+        const targetElem = entries[0].target;
+        // 移除侦听（为了确保以下内容只执行一次）
+        observer.unobserve(entries[0].target);
+        // 为了确保以下内容只执行一次
+        contentWrapperElemRef.value.onscroll = null;
+        // 数据加载完成前，在当前所有渲染的卡片中的最后一个的下一个元素处渲染loading
+        const containerElem = document.createElement("div");
+        // 将Loading组件挂载到containerElem元素上
+        const app = createApp(Loading);
+        app.mount(containerElem);
+        // 将该元素添加到targetElem元素后面，作为其下一个兄弟元素
+        targetElem.insertAdjacentElement("afterend", containerElem);
+        // 获取下一页数据
+        curPage.value++;
+        // 将新的数据添加到旧的数据中，并更新datas，完成视图的更新，同时将合并后的数据存储到localstorage
+        const oldDatas = JSON.parse(getStorage("netflixPageData")!);
+
+        if (oldDatas) {
+          const newDatas: Data = await getNetflixPageData(curPage.value);
+          const curDatas: Data = {
+            imgURLs: [""],
+            videoTitles: [""],
+            videoScores: [""],
+            videoEpisodes: [""],
+          };
+          // 数据合并
+          for (let key in oldDatas) {
+            // @ts-ignore
+            curDatas[key] = oldDatas[key].concat(newDatas[key]);
+          }
+          // 更新datas
+          datas.value = curDatas;
+          // 存储本地
+          setStorage("netflixPageData", JSON.stringify(curDatas));
+        }
+        // 数据加载完成
+        // 移除掉targetElem元素后面的loading元素
+        targetElem.nextSibling!.remove();
+        // 重新进行scroll事件的监听
+        contentWrapperElemRef.value.onscroll = fn;
+      }
+    });
+    observer.observe(
+      videoShowCardElemsRef.value[videoShowCardElemsRef.value.length - 1],
+    );
+  }
 });
 
 const loadingComName = ref("LoadingSkeleton");
@@ -19,22 +93,28 @@ const loadingComName = ref("LoadingSkeleton");
       <h2>Netflix美剧</h2>
       <div class="placeholder"></div>
     </div>
-    <div class="content-wrapper">
+    <div class="content-wrapper" ref="contentWrapperElemRef">
       <div
         class="video-show-card-wrapper"
         v-loading:[loadingComName]="isLoading"
       >
-        <VideoShowCard
+        <div
+          class="video-show-card"
+          ref="videoShowCardElemsRef"
           v-for="(i, index) in datas.videoTitles"
           :key="index"
-          :title="datas.videoTitles[index]"
-          :imgURL="datas.imgURLs[index]"
-          :episode="datas.videoEpisodes[index]"
-          :score="datas.videoScores[index]"
-          is-default
-        />
+          :id="index + ''"
+        >
+          <VideoShowCard
+            :title="datas.videoTitles[index]"
+            :imgURL="datas.imgURLs[index]"
+            :episode="datas.videoEpisodes[index]"
+            :score="datas.videoScores[index]"
+            is-default
+          />
+        </div>
       </div>
-      <div class="pagination-wrapper">
+      <!-- <div class="pagination-wrapper">
         <Pagination
           :page-counts="30"
           @getTargetPageData="
@@ -43,7 +123,7 @@ const loadingComName = ref("LoadingSkeleton");
             }
           "
         />
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
