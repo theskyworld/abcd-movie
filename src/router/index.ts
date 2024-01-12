3618b531bf74e82478eb9dc91245899729daffab
theskyworld <2456450687@qq.com>
2024-01-12 20:06:44 +0800

    chore: add page titles,website icon

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,28 +5,46 @@ const routes = [
   {
     path: "/",
     component: () => import("@/pages/homePage/HomePage.vue"),
+    meta: {
+      title: "ABCD影视",
+    },
   },
   {
     path: "/netflix",
     component: () => import("@/pages/netflixPage/NetflixPage.vue"),
+    meta: {
+      title: "奈飞热播",
+    },
   },
   {
     path: "/movie",
     component: () => import("@/pages/moviePage/MoviePage.vue"),
+    meta: {
+      title: "电影精选",
+    },
   },
   {
     path: "/tv",
     component: () => import("@/pages/tvPage/TVPage.vue"),
+    meta: {
+      title: "电视剧热播",
+    },
   },
   // 动漫
   {
     path: "/anime",
     component: () => import("@/pages/animePage/AnimePage.vue"),
+    meta: {
+      title: "动漫精选",
+    },
   },
   // 综艺
   {
     path: "/variety",
     component: () => import("@/pages/varietyPage/VarietyPage.vue"),
+    meta: {
+      title: "综艺",
+    },
   },
   // 搜索结果
   {
@@ -44,6 +62,13 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title as string;
+  }
+  next();
 });
 
 export default router;
